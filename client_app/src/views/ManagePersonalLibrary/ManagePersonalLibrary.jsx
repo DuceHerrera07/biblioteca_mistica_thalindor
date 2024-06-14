fc4784b2918512c2c1a0a3789849b457a26afabe
DulceHerrera07 <hgdulce267@gmail.com>
2024-06-14 02:44:06 -0600

    Cambios de estilo libreria personal

--- client_app/src/views/ManagePersonalLibrary/ManagePersonalLibrary.jsx
+++ client_app/src/views/ManagePersonalLibrary/ManagePersonalLibrary.jsx
@@ -19,35 +19,37 @@ function ManagePersonalLibrary() {
 
   return (
     <div className="container mt-4">
-      <h1 className="mb-4">Tu Lista</h1>
+      <h1 className="mb-4"><strong>Tu lista</strong></h1>
 
-      <table className="table table-hover">
-        <thead className="table-dark">
-          <tr>
-            <th scope="col">Nombre del Libro</th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {books.map(book => (
-            <tr key={book.id}>
-              <td>
-                <Link to={`/specificBook/${book.id}`} className="text-reset text-decoration-none">{book.title}</Link>
-              </td>
-
-              <td>
-                <ToggleEye />
-              </td>
-    
-              <td>
-                <ButtonDelete onDelete={() => handleDelete(book.id)} />
-              </td>
+      <div className="table-responsive">
+        <table className="table table-hover">
+          <thead className="table-dark">
+            <tr>
+              <th scope="col" className="col-9">Nombre del Libro</th>
+              <th scope="col"></th>
+              <th scope="col"></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {books.map(book => (
+              <tr key={book.id}>
+                <td className="col-9">
+                  <Link to={`/specificBook/${book.id}`} className="text-reset text-decoration-none">{book.title}</Link>
+                </td>
+
+                <td className="col-1 text-center">
+                  <ToggleEye />
+                </td>
+      
+                <td className="col-1 text-center">
+                  <ButtonDelete onDelete={() => handleDelete(book.id)} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
