d4d00e4b625b2f37cd71329d60ae3cf08334f55b
mckimssongg <soymckimsson286@gmail.com>
2024-06-13 20:54:58 -0600

    chore: Update Bootstrap version and import paths for styles

--- client_app/src/App.jsx
+++ client_app/src/App.jsx
@@ -2,11 +2,9 @@ import React, { Suspense, useEffect } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './routes/routerConfig'
 import SpinnerComponent from './components/Spinner/SpinnerComponent'
+import './theme/main.scss'
 
 function App() {
-  useEffect(() => {
-    import('./theme/main.scss');
-  }, []);
   return (
     <BrowserRouter>
         <Suspense fallback={<SpinnerComponent/>}>
